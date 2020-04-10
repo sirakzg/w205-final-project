74554cb07aa7978b060d000cea7a1a72fa64d22d
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-10 19:31:00 +0000

    axe api

--- game_api.py
+++ game_api.py
@@ -2,6 +2,7 @@
 import json
 from kafka import KafkaProducer
 from flask import Flask, request
+from flask_api import status
 
 app = Flask(__name__)
 producer = KafkaProducer(bootstrap_servers='kafka:29092')
@@ -18,9 +19,27 @@ def default_response():
     log_to_kafka('events', default_event)
     return "This is the default response!\n"
 
-
 @app.route("/purchase_a_sword")
 def purchase_a_sword():
-    purchase_sword_event = {'event_type': 'purchase_sword'}
+    if !request.is_json:
+        return "Content not in JSON!",status.HTTP_400_BAD_REQUEST
+    data = request.get_json()
+    
+    purchase_axe_event = {'event_type': 'purchase',
+                          'type'      : 'sword',
+                          'user'      : data['user']}
     log_to_kafka('events', purchase_sword_event)
     return "Sword Purchased!\n"
+
+
+@app.route("/purchase_an_axe",methods = ['POST'])
+def purchase_an_axe():
+    if !request.is_json:
+        return "Content not in JSON!",status.HTTP_400_BAD_REQUEST
+    data = request.get_json()
+    
+    purchase_axe_event = {'event_type': 'purchase',
+                          'type'      : 'axe',
+                          'user'      : data['user']}
+    log_to_kafka('events', purchase_axe_event)
+    return "Axe Purchased!\n"
