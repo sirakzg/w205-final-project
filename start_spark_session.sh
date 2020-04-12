97695a47c992f5ffee108ef6db62a5934e5474ab
jma <jackyma522@gmail.com>
2020-04-12 13:29:40 -0700

    fix fork

--- start_spark_session.sh
+++ start_spark_session.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Starting the purchase spark session"
-nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py > /dev/null 2>&1
+nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py &
 
 
 echo "Starting the user spark session"
-nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_user_stream.py > /dev/null 2>&1
+nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_user_stream.py &
 
 
 echo "Starting the guild spark session"
-nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_guild_stream.py > /dev/null 2>&1
+nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_guild_stream.py &
 
 echo "Starting the record spark session"
-nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_player_record_stream.py > /dev/null 2>&1
+nohop docker-compose exec spark spark-submit /w205/w205-final-project/write_player_record_stream.py &
