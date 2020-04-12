7861851d30319d05ed1b74c0e639bb9cbd6d6092
jma <jackyma522@gmail.com>
2020-04-12 13:22:16 -0700

    add perl script

--- start_spark_session.pl
+++ start_spark_session.pl
@@ -2,4 +2,4 @@
 use strict;
 use warnings;
 
-system(docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py &);
+system("docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py &");
