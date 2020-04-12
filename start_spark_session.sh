9b9196eb553967c124affdbddf90455542168ebd
jma <jackyma522@gmail.com>
2020-04-12 12:50:31 -0700

    fix forking

--- start_spark_session.sh
+++ start_spark_session.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Starting the purchase spark session"
-docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py > /dev/null 2>&1 & disown
+docker-compose exec spark spark-submit /w205/w205-final-project/write_purchase_stream.py > /dev/null 2>&1
 
 echo "Starting the user spark session"
-docker-compose exec spark spark-submit /w205/w205-final-project/write_user_stream.py > /dev/null 2>&1 & disown
+docker-compose exec spark spark-submit /w205/w205-final-project/write_user_stream.py > /dev/null 2>&1
 
 echo "Starting the guild spark session"
-docker-compose exec spark spark-submit /w205/w205-final-project/write_guild_stream.py > /dev/null 2>&1 & disown
+docker-compose exec spark spark-submit /w205/w205-final-project/write_guild_stream.py > /dev/null 2>&1
 
 echo "Starting the record spark session"
-docker-compose exec spark spark-submit /w205/w205-final-project/write_player_record_stream.py > /dev/null 2>&1 & disown
+docker-compose exec spark spark-submit /w205/w205-final-project/write_player_record_stream.py > /dev/null 2>&1 
